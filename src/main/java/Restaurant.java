bf2961c3486d11c2fd7f9f4f1efa3ff92ddacab7
jadhavrajni <jadhavrajni@gmail.com>
2022-10-03 15:23:41 +0530

    First Commit

---
+++ src/main/java/Restaurant.java
@@ -0,0 +1,82 @@
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Restaurant {
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+    public boolean isRestaurantOpen() {
+        LocalTime timeNow=getCurrentTime();
+        int openCheck= timeNow.compareTo(openingTime);
+        int closeCheck= timeNow.compareTo(closingTime);
+        if (openCheck>=0 && closeCheck<0) return true;
+        else return false;
+
+        //return true;
+        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+
+    }
+
+    public LocalTime getCurrentTime(){ return  LocalTime.now(); }
+
+    public List<Item> getMenu() {
+        //return null;
+        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+        return this.menu;
+    }
+
+    private Item findItemByName(String itemName){
+        for(Item item: menu) {
+            if(item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name,price);
+        menu.add(newItem);
+    }
+    
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+    public void displayDetails(){
+        System.out.println("Restaurant:"+ name + "\n"
+                +"Location:"+ location + "\n"
+                +"Opening time:"+ openingTime +"\n"
+                +"Closing time:"+ closingTime +"\n"
+                +"Menu:"+"\n"+getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+
+public double calculateOrderValue(String name1, String name2) {
+    Item item1 = findItemByName(name1);
+    Item item2 = findItemByName(name2);
+    double orderValue = item1.getPrice() + item2.getPrice();
+    return orderValue;
+}
+
+}
---
+++ src/test/java/RestaurantTest.java
@@ -0,0 +1,94 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+
+class RestaurantTest {
+    Restaurant restaurant;
+    //REFACTOR ALL THE REPEATED LINES OF CODE
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>OPEN/CLOSED<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    //-------FOR THE 2 TESTS BELOW, YOU MAY USE THE CONCEPT OF MOCKING, IF YOU RUN INTO ANY TROUBLE
+
+    @BeforeEach
+   public void setup() {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurant = new Restaurant("Amelie's cafe", "Chennai", openingTime, closingTime);
+        restaurant.addToMenu("Sweet corn soup", 119);
+        restaurant.addToMenu("Vegetable lasagne", 269);
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_true_if_time_is_between_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+
+        LocalTime Mocktime= LocalTime.parse("11:00:00");
+        Restaurant RestaurantMock= Mockito.spy(restaurant);
+        when(RestaurantMock.getCurrentTime()).thenReturn(Mocktime);
+        assertTrue(RestaurantMock.isRestaurantOpen());
+    }
+
+    @Test
+    public void is_restaurant_open_should_return_false_if_time_is_outside_opening_and_closing_time(){
+        //WRITE UNIT TEST CASE HERE
+
+        LocalTime Mocktime= LocalTime.parse("23:00:00");
+        Restaurant RestaurantMock= Mockito.spy(restaurant);
+        when(RestaurantMock.getCurrentTime()).thenReturn(Mocktime);
+        assertFalse(RestaurantMock.isRestaurantOpen());
+    }
+
+    //<<<<<<<<<<<<<<<<<<<<<<<<<OPEN/CLOSED>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
+    //>>>>>>>>>>>>>>>>>>>>>>>>>>>MENU<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    @Test
+    public void adding_item_to_menu_should_increase_menu_size_by_1(){
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.addToMenu("Sizzling brownie",319);
+        assertEquals(initialMenuSize+1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_from_menu_should_decrease_menu_size_by_1() throws itemNotFoundException {
+
+        int initialMenuSize = restaurant.getMenu().size();
+        restaurant.removeFromMenu("Vegetable lasagne");
+        assertEquals(initialMenuSize-1,restaurant.getMenu().size());
+    }
+    @Test
+    public void removing_item_that_does_not_exist_should_throw_exception() {
+
+        assertThrows(itemNotFoundException.class,
+                ()->restaurant.removeFromMenu("French fries"));
+    }
+    //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    @Test
+    public void calculate_order_should_return_a_value_greater_than_zero(){
+        String item1= "Sweet corn soup";
+        String item2= "Vegetable lasagne";
+        double costOfOrder= restaurant.calculateOrderValue(item1,item2);
+         assertTrue(costOfOrder>0);
+    }
+
+    @Test
+    public void calculate_order_should_display_an_error_if_the_cost_of_the_order_is_less_than_or_equal_to_zero(){
+        restaurant.addToMenu("Tangy hot tomato soup", 0);
+        restaurant.addToMenu("Vegetable pizza", 0);
+        String item1= "Tangy hot tomato soup";
+        String item2= "Vegetable pizza";
+        double costOfOrder= restaurant.calculateOrderValue(item1,item2);
+        assertTrue(costOfOrder<=0,"Incorrect Order Value calculations");
+    }
+
+
+}
